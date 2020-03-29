6fc50d2abd0ccad8ab1d6f0b0a232d4002acfe21
Karno Sose <karnosose@gmail.com>
2020-03-29 20:10:09 +0400

    Create survey title input

--- src/Components/CreateSurveyBlock/CreateSurveyBlock.js
+++ src/Components/CreateSurveyBlock/CreateSurveyBlock.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+
 
 function CreateSurveyBlock({ classes }) {
-  return <div className={classes.container}>CreateSurveyBlock works!!!</div>;
+  return (
+    <div className={classes.container}>
+      <div>
+        <div>
+          <Typography variant="h4" display="block" gutterBottom="true">
+            Create Survey
+          </Typography>
+        </div>
+
+        <form className={classes.surveyForm} noValidate autoComplete="off">
+          <TextField className={classes.surveyFormItem} label="Survey title" variant="outlined" />
+        </form>
+      </div>
+    </div>
+  )
 }
 
 CreateSurveyBlock.propTypes = {
--- src/Components/CreateSurveyBlock/CreateSurveyBlock.style.js
+++ src/Components/CreateSurveyBlock/CreateSurveyBlock.style.js
@@ -5,9 +5,15 @@ export const styles = {
     display: 'flex',
     justifyContent: 'center',
     borderRadius: `${GLOBAL_STYLES.borders.borderRadius.base}`,
-    boxShadow: `${GLOBAL_STYLES.boxShadow.base}`,
+    // boxShadow: `${GLOBAL_STYLES.boxShadow.base}`,
     boxSizing: 'border-box',
     height: '100%',
     padding: `${GLOBAL_STYLES.spacing.small}`
+  },
+  surveyForm : {
+    width: 600,
+  },
+  surveyFormItem: {
+    width: "100%"
   }
 };
